e3614390bb8265151ed296597ee9c13249aa15cb
Emily-Ann V <emilyann.vdwalt@gmail.com>
2026-05-01 17:25:05 +0200

    Add initial login screen layout

--- app/src/main/java/com/example/texi/MainActivity.kt
+++ app/src/main/java/com/example/texi/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.texi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }

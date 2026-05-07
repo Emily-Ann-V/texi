75390dbfaab98a3fa88be7b66978d49516022958
Emily-Ann V <emilyann.vdwalt@gmail.com>
2026-05-07 09:13:55 +0200

    Update UI styling rules and themes
    Update UI styling rules and themes
    
    Update themes, styles, colours and dimens rules

--- app/src/main/java/com/example/texi/RegisterActivity.kt
+++ app/src/main/java/com/example/texi/RegisterActivity.kt
@@ -1,11 +1,28 @@
 package com.example.texi
 
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val btnRegister = findViewById<Button>(R.id.btn_register_submit)
+
+        btnRegister.setOnClickListener {
+            finish()
+        }
+
+        val backLink = findViewById<TextView>(R.id.tv_login_back_link)
+
+        backLink.paintFlags = backLink.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        backLink.setOnClickListener {
+            finish()
+        }
     }
 }

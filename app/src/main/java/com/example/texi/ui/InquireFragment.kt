2dd607bda2fc20b134f58162822534016abb1439
Emily-Ann V <emilyann.vdwalt@gmail.com>
2026-05-19 20:41:34 +0200

    Add inquire functionality

---
+++ app/src/main/java/com/example/texi/ui/InquireFragment.kt
@@ -0,0 +1,33 @@
+package com.example.texi.ui
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.texi.R
+
+
+class InquireFragment : Fragment(R.layout.fragment_inquire) {
+
+    override fun onViewCreated(view: View, savedInstanceState : Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ibSendMessage = view.findViewById<ImageButton>(R.id.ib_inquire_send_message_icon)
+        val etType = view.findViewById<EditText>(R.id.et_inquire_type)
+
+        ibSendMessage.setOnClickListener {
+            val typeInput = etType.text.toString().trim()
+            val tvMessage = view.findViewById<TextView>(R.id.tv_inquire_message)
+
+            if (typeInput.isNotEmpty()) {
+                tvMessage.text = typeInput
+                tvMessage.visibility = View.VISIBLE
+            } else {
+                Toast.makeText(requireContext(), "Please type a message.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/texi/ui/TextbookDetailsFragment.kt
+++ app/src/main/java/com/example/texi/ui/TextbookDetailsFragment.kt
@@ -2,6 +2,7 @@ package com.example.texi.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -22,6 +23,7 @@ class TextbookDetailsFragment : Fragment(R.layout.fragment_textbook_details) {
         val field = arguments?.getString("field")
         val degree = arguments?.getString("degree")
 
+        val btnInquire = view.findViewById<Button>(R.id.btn_inquire_page)
         val ivImage = view.findViewById<ImageView>(R.id.iv_textbook_details_image)
         val tvTitle = view.findViewById<TextView>(R.id.tv_textbook_details_title)
         val tvAuthor = view.findViewById<TextView>(R.id.tv_textbook_details_author)
@@ -31,6 +33,13 @@ class TextbookDetailsFragment : Fragment(R.layout.fragment_textbook_details) {
         val tvUniversity = view.findViewById<TextView>(R.id.tv_textbook_details_university)
         val tvField = view.findViewById<TextView>(R.id.tv_textbook_details_field)
         val tvDegree = view.findViewById<TextView>(R.id.tv_textbook_details_degree)
+
+        btnInquire.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fl_main, InquireFragment())
+                .addToBackStack(null)
+                .commit()
+        }
 
         ivImage.setImageResource(imageResId ?: 0)
         tvTitle.text = title
